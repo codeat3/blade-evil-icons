1a8c60235a151fe6be4b8c4f85a47e5d0428d49d
swapnilsarwe <swapnilsarwe@users.noreply.github.com>
2022-07-26 05:13:53 +0000

    Fix styling

--- src/BladeEvilIconsServiceProvider.php
+++ src/BladeEvilIconsServiceProvider.php
@@ -17,24 +17,24 @@ final class BladeEvilIconsServiceProvider extends ServiceProvider
         $this->callAfterResolving(Factory::class, function (Factory $factory, Container $container) {
             $config = $container->make('config')->get('blade-evil-icons', []);
 
-            $factory->add('evil-icons', array_merge(['path' => __DIR__.'/../resources/svg'], $config));
+            $factory->add('evil-icons', array_merge(['path' => __DIR__ . '/../resources/svg'], $config));
         });
     }
 
     private function registerConfig(): void
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/blade-evil-icons.php', 'blade-evil-icons');
+        $this->mergeConfigFrom(__DIR__ . '/../config/blade-evil-icons.php', 'blade-evil-icons');
     }
 
     public function boot(): void
     {
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../resources/svg' => public_path('vendor/blade-evil-icons'),
+                __DIR__ . '/../resources/svg' => public_path('vendor/blade-evil-icons'),
             ], 'blade-ei'); // TODO: change this alias to `blade-evil-icons` in the next major release
 
             $this->publishes([
-                __DIR__.'/../config/blade-evil-icons.php' => $this->app->configPath('blade-evil-icons.php'),
+                __DIR__ . '/../config/blade-evil-icons.php' => $this->app->configPath('blade-evil-icons.php'),
             ], 'blade-evil-icons-config');
         }
     }
